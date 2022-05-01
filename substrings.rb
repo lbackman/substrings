94ec44fa0fd4a89eaf46bb28c77786efa16fbd39
Lucas Bäckman <lucas.backman@gmail.com>
2022-05-01 12:23:08 +0300

    Define method that counts substring occurences

--- substrings.rb
+++ substrings.rb
@@ -1,25 +1,38 @@
-# Take in a string as argument and separate it in to an array of downcase
-# with punctuation removed.
-# Then, convert all words in to arrays themselves.
-# Finally build all combinations of substrings from consecutive elements
-# in the arrays and recombine them to words.
+dictionary = ["below","down","go","going","horn","how","howdy","it",
+              "i","low","own","part","partner","sit"]
 
-s = "Never in my life."
-# Split string in to array with punctuation removed
-a = s.downcase.gsub(/\W+/, " ").split(" ")
-substring_array = []
-combos = []
-a.each do |word|
-  i = 0
-  len = word.length
-  while i < len
-    j = len - (len - i)
-    while j < len
-      combos << word[i, len - j]
-      j += 1
-    end
-    i += 1
-  end
-  substring_array << combos
+def get_substrings(arr)
+  substring_array = []
   combos = []
+  arr.each do |word|
+    i = 0
+    len = word.length
+    while i < len
+      j = len - (len - i)
+      while j < len
+        combos << word[i, len - j]
+        j += 1
+      end
+      i += 1
+    end
+    substring_array << combos
+    combos = []
+  end
+  substring_array.flatten
 end
+
+def substrings(string, words)
+  arr = string.downcase.gsub(/\W+/, " ").split(" ")
+  substrings = get_substrings(arr)
+  hash = Hash.new(0)
+  words.each do |w|
+    if substrings.count(w) > 0
+      hash[w] = substrings.count(w)
+    end
+  end
+  puts hash
+end
+
+puts "Give a word or phrase to check for substrings."
+str = gets.chomp
+substrings(str, dictionary)
